8c02a4faad1dd28e3ce5891ab8dfed0c1790ade5
Eric Latham <ericoliverlatham@gmail.com>
2020-11-11 21:58:34 -0600

    format

--- src/entity/adapter/drone/physical/TelloDroneAdapter.java
+++ src/entity/adapter/drone/physical/TelloDroneAdapter.java
@@ -1,9 +1,8 @@
 package entity.adapter.drone.physical;
 
-import java.io.IOException;
-
 import constants.Constants;
 import entity.adapter.drone.virtual.AnimatedDroneInterface;
+import java.io.IOException;
 
 public class TelloDroneAdapter implements AnimatedDroneInterface {
   private TelloDrone telloDrone;
@@ -20,56 +19,91 @@ public class TelloDroneAdapter implements AnimatedDroneInterface {
 
   }
 
-  public void scanFarm() throws IOException {
-    // TODO: make physical drone do what our animated drone does
+  public void scanFarm() {
     if (isDeployed()) return;
 
-    // unsure where to apply this
-    int flightFloor = Constants.ITEM_HEIGHT_MAX / 25; // converted to feet
+    try {
+      // unsure where to apply this
+      int flightFloor = Constants.ITEM_HEIGHT_MAX / 25; // converted to feet
 
-    int travelVerticalDistance = Constants.REAL_DRONE_Y_BOUND;
-    int travelLeftDistance = Constants.REAL_DRONE_X_BOUND;
-    int travelRightDistance = Constants.REAL_DRONE_X_BOUND / 5;
+      int travelVerticalDistance = Constants.REAL_DRONE_Y_BOUND;
+      int travelLeftDistance = Constants.REAL_DRONE_X_BOUND;
+      int travelRightDistance = Constants.REAL_DRONE_X_BOUND / 5;
 
-    // initial turn
-	telloDrone.turnCCW(90);
-    System.out.println("The drone turns 90 degrees counter clockwise");
+      // initial turn
+      telloDrone.turnCCW(90);
+      System.out.println("The drone turns 90 degrees counter clockwise");
 
-    for (int i = 0; i < 2; i++) {
-    	telloDrone.flyForward(travelVerticalDistance);
-        System.out.println("The drone travels forward " + travelVerticalDistance + " feet to the bottom of the farm");
+      for (int i = 0; i < 2; i++) {
+        telloDrone.flyForward(travelVerticalDistance);
+        System.out.println(
+          "The drone travels forward " +
+          travelVerticalDistance +
+          " feet to the bottom of the farm"
+        );
         telloDrone.turnCCW(90);
         System.out.println("The drone turns 90 degrees counter clockwise");
-    	telloDrone.flyForward(travelRightDistance);
-        System.out.println("The drone travels forward " + travelRightDistance + " feet to the right");
+        telloDrone.flyForward(travelRightDistance);
+        System.out.println(
+          "The drone travels forward " +
+          travelRightDistance +
+          " feet to the right"
+        );
         telloDrone.turnCCW(90);
         System.out.println("The drone turns 90 degrees counter clockwise");
-    	telloDrone.flyForward(travelVerticalDistance);
-        System.out.println("The drone travels forward " + travelVerticalDistance + " feet to the top of the farm");
+        telloDrone.flyForward(travelVerticalDistance);
+        System.out.println(
+          "The drone travels forward " +
+          travelVerticalDistance +
+          " feet to the top of the farm"
+        );
         telloDrone.turnCW(90);
         System.out.println("The drone turns 90 degree clockwise");
-    	telloDrone.flyForward(travelRightDistance);
-        System.out.println("The drone travels forward " + travelRightDistance + " feet to the right");
+        telloDrone.flyForward(travelRightDistance);
+        System.out.println(
+          "The drone travels forward " +
+          travelRightDistance +
+          " feet to the right"
+        );
         telloDrone.turnCW(90);
         System.out.println("The drone turns 90 degree clockwise");
+      }
+
+      telloDrone.flyForward(travelVerticalDistance);
+      System.out.println(
+        "The drone travels forward " +
+        travelVerticalDistance +
+        " feet to the bottom of the farm"
+      );
+      telloDrone.turnCCW(90);
+      System.out.println("The drone turns 90 degrees counter clockwise");
+      telloDrone.flyForward(travelRightDistance);
+      System.out.println(
+        "The drone travels forward " +
+        travelRightDistance +
+        " feet to the right"
+      );
+      telloDrone.turnCCW(90);
+      System.out.println("The drone turns 90 degrees counter clockwise");
+      telloDrone.flyForward(travelVerticalDistance);
+      System.out.println(
+        "The drone travels forward " +
+        travelVerticalDistance +
+        " feet to the top of the farm"
+      );
+      // at top right of farm here
+
+      telloDrone.turnCCW(90);
+      System.out.println("The drone turns 90 degrees counter clockwise");
+      telloDrone.flyForward(travelLeftDistance);
+      System.out.println(
+        "The drone travels forward " +
+        travelLeftDistance +
+        " feet to the left back to the origin"
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    telloDrone.flyForward(travelVerticalDistance);
-    System.out.println("The drone travels forward " + travelVerticalDistance + " feet to the bottom of the farm");
-    telloDrone.turnCCW(90);
-    System.out.println("The drone turns 90 degrees counter clockwise");
-	telloDrone.flyForward(travelRightDistance);
-    System.out.println("The drone travels forward " + travelRightDistance + " feet to the right");
-    telloDrone.turnCCW(90);
-    System.out.println("The drone turns 90 degrees counter clockwise");
-	telloDrone.flyForward(travelVerticalDistance);
-    System.out.println("The drone travels forward " + travelVerticalDistance + " feet to the top of the farm");
-    // at top right of farm here
-
-    telloDrone.turnCCW(90);
-    System.out.println("The drone turns 90 degrees counter clockwise");
-    telloDrone.flyForward(travelLeftDistance);
-    System.out.println("The drone travels forward " + travelLeftDistance + " feet to the left back to the origin");
   }
 
   public boolean isDeployed() {
